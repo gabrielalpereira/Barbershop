8f9eba6c18a61bcc9719a17ce25f14e66a1e14d4
Gabriela <gabezinhalp@gmail.com>
2024-01-31 13:11:05 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
   return (
@@ -9,18 +10,22 @@ export default function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-      <h2 className="text-xl font-bold">Olá, Miguel!</h2>
-      <p className="capitalize text-sm">
-        {format(new Date(), "EEEE',' dd 'de' MMMM", {
-        locale: ptBR,
-      })}
-      </p>
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
       </div>
 
       <div className="px-5 mt-6">
-      <Search/>
+        <Search />
       </div>
 
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem />
+      </div>
     </div>
   );
 }
