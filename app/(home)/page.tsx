8a08b4d2e80036b8d3e118f067d0ba7c737a872f
Gabriela <gabezinhalp@gmail.com>
2024-01-31 14:58:20 -0300

    feat: add imageUrl to service

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,8 +8,8 @@ import BarbershopItem from "./_components/barbershop-item";
 
 export default async function Home() {
   //Chamar prisma e pegar barbearias
-  const barbershops = await db.barbershop.findMany({})
-  
+  const barbershops = await db.barbershop.findMany({});
+
   return (
     <div>
       <Header />
@@ -28,32 +28,35 @@ export default async function Home() {
       </div>
 
       <div className="px-5 mt-6">
-        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
+          Agendamentos
+        </h2>
         <BookingItem />
       </div>
 
-        <div className="mt-6">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
-        
+      <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
+
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+      </div>
 
-        </div>    
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
 
-        <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
-        
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
-
-        </div>   
-
+      </div>
     </div>
   );
 }
