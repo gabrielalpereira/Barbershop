2dbd5b79600c8ae308631888d7298594208df55d
Gabriela <gabezinhalp@gmail.com>
2024-02-03 16:19:56 -0300

    fix: services type

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,6 +3,7 @@ import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/_lib/auth";
+import { Key } from "react";
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -37,7 +38,7 @@ const BarbershopDetailsPage = async ({
     <div>
       <BarbershopInfo barbershop={barbershop} />
       <div className="px-5 flex flex-col gap-4 py-6">
-        {barbershop.services.map((service) => (
+        {barbershop.services.map((service: { id: Key | null | undefined }) => (
           <ServiceItem
             key={service.id}
             barbershop={barbershop}
