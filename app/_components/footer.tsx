2ec9fb6cd11c63053689f173e56dd56d622288d7
Gabriela <gabezinhalp@gmail.com>
2024-02-04 12:45:22 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return ( 
-        <div className="w-full bg-secondary py-6 px-5">
-            <p className="text-gray-400 text-xs font-bold opacity-75">@ 2023 Copyright FSW Barber</p>
-        </div>
-     );
-}
- 
+  return (
+    <footer className="w-full bg-secondary py-6 px-5">
+      <p className="text-gray-400 text-xs font-bold opacity-75">
+        @ 2023 Copyright FSW Barber
+      </p>
+    </footer>
+  );
+};
+
 export default Footer;
